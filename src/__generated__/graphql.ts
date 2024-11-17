cee5b44a2e832c9a8a4ee372ed064a450308ba32
Lukas Karas <lukaskaras.lk@gmail.com>
2024-11-17 10:35:24 +0100

    Add graphql

---
+++ src/__generated__/graphql.ts
@@ -0,0 +1,59 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+};
+
+export type BasicInfo = {
+  __typename?: 'BasicInfo';
+  age: Scalars['Int']['output'];
+  bloodType?: Maybe<Scalars['String']['output']>;
+  gender: Scalars['String']['output'];
+  name: Scalars['String']['output'];
+};
+
+export type MedicalHistory = {
+  __typename?: 'MedicalHistory';
+  allergies: Array<Scalars['String']['output']>;
+  chronicConditions: Array<Scalars['String']['output']>;
+  currentMedications: Array<Scalars['String']['output']>;
+};
+
+export type Patient = {
+  __typename?: 'Patient';
+  basicInfo: BasicInfo;
+  medicalHistory: MedicalHistory;
+  recentVitals: RecentVitals;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  patient?: Maybe<Patient>;
+};
+
+export type RecentVitals = {
+  __typename?: 'RecentVitals';
+  bloodPressure: Scalars['String']['output'];
+  heartRate: Scalars['Int']['output'];
+  temperature: Scalars['Float']['output'];
+};
+
+export type GetPatientQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetPatientQuery = { __typename?: 'Query', patient?: { __typename?: 'Patient', basicInfo: { __typename?: 'BasicInfo', name: string, age: number, gender: string, bloodType?: string | null } } | null };
+
+
+export const GetPatientDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"getPatient"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"patient"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"basicInfo"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"age"}},{"kind":"Field","name":{"kind":"Name","value":"gender"}},{"kind":"Field","name":{"kind":"Name","value":"bloodType"}}]}}]}}]}}]} as unknown as DocumentNode<GetPatientQuery, GetPatientQueryVariables>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,34 @@
 import Image from "next/image";
-import localFont from "next/font/local";
 import { Container } from "@/components/container";
-import { Item } from "@/components/item";
 import {
   ClipboardDocumentListIcon,
   HeartIcon,
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import { Card } from "@/components/card";
+import { useQuery } from "@apollo/client";
+import { gql } from "@/__generated__/gql";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const QUERY = gql(`
+  query getPatient {
+    patient {
+      basicInfo {
+        name
+        age
+        gender
+        bloodType
+      }
+    }
+  }
+`);
 
 export default function Home() {
+  const { loading, error, data } = useQuery(QUERY);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error || !data?.patient) return <p>Something went wrong</p>;
+
   return (
     <div className="grid grid-cols-[minmax(0,_400px)_1fr] gap-4 p-4">
       <div className="flex flex-col gap-4">
@@ -29,9 +37,13 @@ export default function Home() {
             icon={<UserCircleIcon className="w-8 h-8 text-gray-500" />}
             heading="Patient information"
             rows={[
-              { title: "Name", text: "John Doe" },
-              { title: "Age", text: "25" },
-              { title: "Gender", text: "Male" },
+              { title: "Name", text: data.patient.basicInfo.name },
+              { title: "Age", text: data.patient.basicInfo.age.toString() },
+              { title: "Gender", text: data.patient.basicInfo.gender },
+              {
+                title: "Blood type",
+                text: data.patient.basicInfo.bloodType || "Unknown",
+              },
             ]}
           />
         </Container>
@@ -62,7 +74,7 @@ export default function Home() {
       </div>
       <Container>
         <div className="flex justify-center min-h-96 min-w-96">
-          <Image src="/brain.png" alt="brain" width={800} height={800} />
+          <Image src="/brain.png" alt="brain" width={600} height={600} />
         </div>
       </Container>
     </div>
